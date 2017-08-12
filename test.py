6533feb297fec37053fcb8a9bbced8051e2e038a
Morissey <stewy.aj@gmail.com>
2017-08-12 17:26:52 +1000

    This is the next message

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ This prints out a variable
 BOYS = ['Lachs', 'Will', 'Tom']
 for w in BOYS:
     print(w, len(w))
- # adding this line before commit
- # added second line to fork
- 
+# adding this line before commit
+# added second line to fork
+# this is the second commit
